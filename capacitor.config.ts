b9a92bd6d408e150562437e9bab59f97fc5b107a
sumit783 <sumitmaha783@gmail.com>
2026-03-08 12:37:00 +0530

    chore: Configure Capacitor to use a remote server URL instead of a local web directory.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,9 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.trjara.app',
   appName: 'Trjara',
-  webDir: 'dist'
+  server: {
+    url: 'https://trjara-user-application.vercel.app'
+  }
 };
 
 export default config;
